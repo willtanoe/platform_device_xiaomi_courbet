978729e038bc2905662fb6503fb537cb042f1206
HELLBOY017 <abhaygill017@gmail.com>
2022-09-28 12:37:38 +0530

    Import OnePlus Dolby
    These Commits are used to add Oneplus  Dolby

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,12 +18,13 @@ TARGET_OTA_ASSERT_DEVICE := courbet,courbetin
 
 # Audio
 TARGET_PROVIDES_AUDIO_EXTNS := true
+AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP := true
 
 # Compression
 PRODUCT_FS_COMPRESSION := 1
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/device_framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/device_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/lineage_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 ODM_MANIFEST_SKUS += courbet
