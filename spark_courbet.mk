97d92d960ef89ebaf5f0709f4fdb27aa857faa96
Willtanoe <willtanu86@gmail.com>
2022-11-03 22:40:06 +0700

    Update and rename aosp_courbet.mk to spark_courbet.mk

--- spark_courbet.mk
+++ spark_courbet.mk
@@ -12,13 +12,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_courbet
+PRODUCT_NAME := spark_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 4G
