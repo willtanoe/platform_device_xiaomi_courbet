a147ff74b0fe8e829eceadc018e26149305b49de
willtanoe <willtanu86@gmail.com>
2022-11-18 19:51:53 +0000

    Awaken Bringup

--- awaken_courbet.mk
+++ awaken_courbet.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from courbet device
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+# Inherit some common Awaken stuff.
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
 PRODUCT_NAME := spark_courbet
 PRODUCT_DEVICE := courbet
