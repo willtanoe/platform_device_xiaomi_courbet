0648e8e476559d8dc03baded62b971536cc137e5
willtanoe <willtanu86@gmail.com>
2022-11-18 19:27:56 +0000

    spark bringup

--- device.mk
+++ device.mk
@@ -72,7 +72,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-spark
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
