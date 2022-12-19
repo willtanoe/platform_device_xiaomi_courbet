e3f0aaf0229a2ed214182767fa17000bb1003f33
willtanoe <willtanu86@gmail.com>
2022-12-19 20:56:51 +0800

    Blaze Adapt

--- blaze_courbet.mk
+++ blaze_courbet.mk
@@ -11,14 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from courbet device
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
-# Inherit some common Awaken stuff.
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+# Inherit from the Blaze configuration.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
-PRODUCT_NAME := awaken_courbet
+PRODUCT_NAME := blaze_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 4G
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Blaze Stuff
+BLAZE_MAINTAINER := willtanoe
+TARGET_USE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Blur
 TARGET_USES_BLUR := true
