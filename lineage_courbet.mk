980f2e260ace0de61a80180d2987338c43a3843b
willtanoe <willtanu86@gmail.com>
2022-12-20 20:02:56 +0800

    Lineage Bringup

--- lineage_courbet.mk
+++ lineage_courbet.mk
@@ -12,16 +12,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := spark_courbet
+PRODUCT_NAME := lineage_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 4G
 PRODUCT_MANUFACTURER := Xiaomi
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-ro.spark.maintainer=willtanoe
 
 # Blur
 TARGET_USES_BLUR := true
@@ -32,14 +29,12 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
-# Pixel Stuff
-USE_PIXEL_CHARGER := false
-
 # Quick Tap
 TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 SELINUX_IGNORE_NEVERALLOWS := true
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="courbet_global-user 12 RKQ1.210614.002 V13.0.8.0.SKQMIXM release-keys"
